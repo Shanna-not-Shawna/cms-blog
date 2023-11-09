bfba4a173b9a33e366818d6f2d1d8ea610d7ea79
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-11-08 22:06:09 -0600

    Added routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,139 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//For looking at individual post
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User, {
+          model: Comment,
+          include: [
+            User
+          ]
+        },
+
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("/newpost", (req, res) => {
+
+
+
+  res.render("newpost");
+});
+
+
+//withAuth required to view profile
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: Post
+        },
+      ],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("post/:id", async (req, res) => {
+  try {
+    const commentData = await Comment.findByPk(req.params.id, {
+      include: [
+        User, {
+          model: Comment,
+          include: [
+            User
+          ]
+        },
+        {
+          model: Post
+        }
+      ],
+    });
+
+    const comment = commentData.get({ plain: true });
+    res.render("post", {
+      ...comment,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+router.get("/login", (req, res) => {
+  // If user is logged in, redirect to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/createAccount", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("createAccount");
+});
+
+module.exports = router;
--- controllers/named-routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { Model1, Model2 } = require('../models');
-
-// GET routes
-
-module.exports = router;
